92049cd0d9a4cb9142a3c3bacc07ec4a5dc9a392
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-13 21:56:32 +0000

    sending a name to the server

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+const connect = function (){
+  const conn = net.createConnection({
+    host : '10.0.2.15',
+    port : 50541
+
+  });
+conn.setEncoding('utf8');
+conn.on('connect', () => {
+  console.log('Sucessfully connected to game server');
+  conn.write('Name: NAD');
+
+})
+conn.on('data', (data) => {
+  console.log('Server says : ', data);
+})
+
+ return conn;
+};
+
+module.exports = {
+  connect,
+};
